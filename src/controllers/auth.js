e70ed637ba1323b58c8f2384cf7fe8d763ee597c
ALeksiiHrabovets <hrabovets78@gmail.com>
2025-09-08 20:19:37 +0300

    fix: replace real secrets with placeholders

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,6 +4,8 @@ import {
   logoutUser,
   refreshUsersSession,
   registerUser,
+  requestResetToken,
+  resetPassword,
 } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
@@ -74,5 +76,28 @@ export const refreshUsersSessionController = async (req, res) => {
     data: {
       accessToken: session.accessToken,
     },
+  });
+};
+
+export const requestResetEmailController = async (req, res) => {
+  try {
+    await requestResetToken(req.body.email);
+    res.json({
+      message: 'Reset password email was successfully sent!',
+      status: 200,
+      data: {},
+    });
+  } catch (err) {
+    console.log('❌ send-reset-email error:', err); // ← добавь это
+    throw err;
+  }
+};
+
+export const resetPasswordController = async (req, res) => {
+  await resetPassword(req.body);
+  res.json({
+    status: 200,
+    message: 'Password has been successfully reset.',
+    data: {},
   });
 };
